735ce5eeb631902627a1d994a22f434dfb3af28b
Matt Ruiz <mfruiz33@gmail.com>
2021-11-12 13:44:26 -0500

    changed rsyncStecUpgrade.sh as we are using the hostname oldstec and rsa keys. So no need to enter password each time

--- rsyncStecUpgrade.sh
+++ rsyncStecUpgrade.sh
@@ -3,59 +3,55 @@
 #  rsync's over the files from old server to new.
 #  
 #  Written by: Matt Ruiz
-#  Note: You will have to enter the password each time
-#        If you don't want to do this you can try using shell keys for authentication
-#        it should default to shell keys anyway as it is more secure
+#  
+#  Edit Date: 11/12/21      
+#        -removed input of ip, we will use oldstec from hostfile. as well as a note that was no longer relevant
 #
-
-
-echo "Enter the Original Stec's IP: "
-read OrigStecIP
 
 echo "######################"
 echo "syncing over rptarc..."
 echo "######################"
 
-rsync -azvh root@$OrigStecIP:/rptarc/ /rptarc/
+rsync -azvh root@oldstec:/rptarc/ /rptarc/
 
 echo "######################"
 echo "syncing over stec..."
 echo "######################"
 
-rsync -azvh --exclude 'hardware/' root@$OrigStecIP:/stec/ /stec/
+rsync -azvh --exclude 'hardware/' root@oldstec:/stec/ /stec/
 
 echo "########################################"
 echo "syncing over /stec/hardware/scripting..."
 echo "########################################"
 
 mkdir /stec/hardware/scripting
-rsync -azvh root@$OrigStecIP:/stec/hardware/scripting/ /stec/hardware/scripting/
+rsync -azvh root@oldstec:/stec/hardware/scripting/ /stec/hardware/scripting/
 
-echo "###########################################################################################"
+echo "############################################################"
 echo "syncing over stecbi, /usr/dlc/, /etc/reliance.env, /usr/admin/.profile and teller/.profile"
-echo "###########################################################################################"
+echo "############################################################"
 
-rsync -azvh root@$OrigStecIP:/stecbi/ /stecbi/
-rsync -azvh root@$OrigStecIP:/usr/dlc/ /usr/dlc/
-rsync -asvh root@$OrigStecIP:/etc/reliance.env /etc/reliance.env
-rsync -asvh root@$OrigStecIP:/usr/admin/.profile /usr/admin/.profile
-rsync -asvh root@$OrigStecIP:/usr/teller/.profile /usr/teller/.profile
+rsync -azvh root@oldstec:/stecbi/ /stecbi/
+rsync -azvh root@oldstec:/usr/dlc/ /usr/dlc/
+rsync -asvh root@oldstec:/etc/reliance.env /etc/reliance.env
+rsync -asvh root@oldstec:/usr/admin/.profile /usr/admin/.profile
+rsync -asvh root@oldstec:/usr/teller/.profile /usr/teller/.profile
 
 echo "########################################"
 echo "syncing over /tmp/comm and /tmp/relrpt..."
 echo "########################################"
 
-rsync -azvh root@$OrigStecIP:/tmp/comm/ /tmp/comm/
-rsync -azvh root@$OrigStecIP:/tmp/relrpt/ /tmp/relrpt/
+rsync -azvh root@oldstec:/tmp/comm/ /tmp/comm/
+rsync -azvh root@oldstec:/tmp/relrpt/ /tmp/relrpt/
 
 echo "########################################"
 echo "syncing over /root/.ssh/*..."
 echo "########################################"
 
-rsync -azvh root@$OrigStecIP:/root/.ssh/id* /root/.ssh/
+rsync -azvh root@oldstec:/root/.ssh/id* /root/.ssh/
 
 echo "########################################"
 echo "syncing over /v10-image/..."
 echo "########################################"
 
-rsync -azvh root@$OrigStecIP:/v10-image/ /v10-image/
+rsync -azvh root@oldstec:/v10-image/ /v10-image/
